154a5e24f04a2fdf13a70d503e22207ecb63e81c
Ilya Shestakov <siavol@gmail.com>
2024-08-06 23:18:57 +0300

    journal repo draft

--- src/services/journal-repository.ts
+++ src/services/journal-repository.ts
@@ -1,24 +1,40 @@
+import { getRepositoryContent } from './github'
+import { Config } from './config-storage'
+
 export type JournalRecord = {
   name: string
   path: string
   htmlUrl: URL
 }
 
-export async function getRecords(): Promise<JournalRecord[]> {
-  return [
-    {
-      name: '20240701-1902',
-      path: 'journal/2024/07/20240701-1902.md',
-      htmlUrl: new URL(
-        'https://github.com/siavol/dairy/blob/main/journal/2024/07/20240701-1902.md'
-      ),
-    },
-    {
-      name: '20240703-0003',
-      path: 'journal/2024/07/20240703-0003.md',
-      htmlUrl: new URL(
-        'https://github.com/siavol/dairy/blob/main/journal/2024/07/20240703-0003.md'
-      ),
-    },
-  ]
+export async function getRecords(config: Config): Promise<JournalRecord[]> {
+  const rootJournalPath = 'journal'
+
+  const yearItems = await getRepositoryContent(rootJournalPath, config)
+  const monthTasks = yearItems.map(async (year) => {
+    return await getRepositoryContent(year.path, config)
+  })
+  const months = (await Promise.all(monthTasks))
+    .flat()
+    .sort()
+
+  const result: JournalRecord[] = []
+
+  return result
+  // return [
+  //   {
+  //     name: '20240701-1902',
+  //     path: 'journal/2024/07/20240701-1902.md',
+  //     htmlUrl: new URL(
+  //       'https://github.com/siavol/dairy/blob/main/journal/2024/07/20240701-1902.md'
+  //     ),
+  //   },
+  //   {
+  //     name: '20240703-0003',
+  //     path: 'journal/2024/07/20240703-0003.md',
+  //     htmlUrl: new URL(
+  //       'https://github.com/siavol/dairy/blob/main/journal/2024/07/20240703-0003.md'
+  //     ),
+  //   },
+  // ]
 }
