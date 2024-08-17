48cc0551d82c4c79b6c7414f24603627052f6d9e
Ilya Shestakov <siavol@gmail.com>
2024-08-17 14:51:12 +0300

    handle callback error

--- src/components/config/config-github-callback.tsx
+++ src/components/config/config-github-callback.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 const ConfigGithubCallback: React.FunctionComponent = () => {
   const [searchParams] = useSearchParams()
+  const { t } = useTranslation('config-github-callback')
 
   const gitHubCode = searchParams.get('code')
+  const gitHubError = searchParams.get('error')
+  const gitHubErrorDescription = searchParams.get('error_description')
 
-  return <div>Code is: {gitHubCode}</div>
+  if (!gitHubError) {
+    return <div>Code is: {gitHubCode}</div>
+  } else {
+    return (
+      <div>
+        <h1>{t('GitHub authentication failed!')}</h1>
+        <p>
+          <strong>Error: </strong>
+          <span>{gitHubError}</span>
+        </p>
+        <p>
+          <strong>Description: </strong>
+          <span>{gitHubErrorDescription}</span>
+        </p>
+      </div>
+    )
+  }
 }
 
 export default ConfigGithubCallback
