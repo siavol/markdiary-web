9da8fcc9ba6fdb0b54b1404735cc94ea94c51848
Ilya Shestakov <siavol@gmail.com>
2024-08-26 00:13:00 +0300

    layout mui update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Link, Outlet, useNavigation } from 'react-router-dom'
+import { Link as RouterLink, Outlet, useNavigation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import Box from '@mui/material/Box'
+import { AppBar, Container, Typography, Button, Toolbar } from '@mui/material'
 
 const Layout: React.FunctionComponent = () => {
   const navigation = useNavigation()
@@ -8,31 +10,35 @@ const Layout: React.FunctionComponent = () => {
 
   if (navigation.state === 'idle') {
     return (
-      <div>
-        <header>Markdairy</header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">{t('Home')}</Link>
-            </li>
-            <li>
-              <Link to="/new">{t('New record')}</Link>
-            </li>
-            <li>
-              <Link to="/config">{t('Configuration')}</Link>
-            </li>
-          </ul>
-        </nav>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Markdairy
+            </Typography>
 
-        <hr />
-        <Outlet />
-      </div>
+            <Button to="/" color="inherit" component={RouterLink}>
+              {t('Home')}
+            </Button>
+            <Button to="/new" color="inherit" component={RouterLink}>
+              {t('New record')}
+            </Button>
+            <Button to="/config" color="inherit" component={RouterLink}>
+              {t('Configuration')}
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Container maxWidth="md">
+          <Outlet />
+        </Container>
+      </Box>
     )
   } else {
     return (
-      <div>
+      <Box>
         <p>{t('loading...', { ns: 'general' })}</p>
-      </div>
+      </Box>
     )
   }
 }
