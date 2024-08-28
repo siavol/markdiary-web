69c9a1b497e3c41c434851381330989f8150e3e7
Ilya Shestakov <siavol@gmail.com>
2024-08-28 23:59:39 +0300

    add floating button to the records list

--- src/components/record/records-list.tsx
+++ src/components/record/records-list.tsx
@@ -1,23 +1,40 @@
 import React from 'react'
 import { JournalRecord } from '../../services/journal-repository'
 import { Link as RouterLink, useLoaderData } from 'react-router-dom'
-import { List, ListItemButton, ListItemText } from '@mui/material'
+import {
+  Container,
+  Fab,
+  List,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
 
 const RecordsList: React.FunctionComponent = () => {
   const data = useLoaderData() as JournalRecord[]
 
   return (
-    <List>
-      {data.map((r) => (
-        <ListItemButton
-          key={r.path}
-          to={`view/${r.name}`}
-          component={RouterLink}
-        >
-          <ListItemText>{r.name}</ListItemText>
-        </ListItemButton>
-      ))}
-    </List>
+    <Container>
+      <List>
+        {data.map((r) => (
+          <ListItemButton
+            key={r.path}
+            to={`view/${r.name}`}
+            component={RouterLink}
+          >
+            <ListItemText>{r.name}</ListItemText>
+          </ListItemButton>
+        ))}
+      </List>
+      <Fab
+        color="primary"
+        sx={{ position: 'fixed', bottom: 32, right: 32 }}
+        to="/new"
+        component={RouterLink}
+      >
+        <AddIcon />
+      </Fab>
+    </Container>
   )
 }
 
