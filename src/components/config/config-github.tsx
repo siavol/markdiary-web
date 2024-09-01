42043f92731842a962b0ad4917db56cec19caf53
Ilya Shestakov <siavol@gmail.com>
2024-09-02 00:10:40 +0300

    migrate config to mui controls

--- src/components/config/config-github.tsx
+++ src/components/config/config-github.tsx
@@ -1,6 +1,17 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Config, loadConfig, saveConfig } from '../../services/config-storage'
 import { useTranslation } from 'react-i18next'
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Link,
+  Typography,
+} from '@mui/material'
 
 const ConfigGithub: React.FunctionComponent = () => {
   const [config, setConfig] = useState<Config>({
@@ -41,84 +52,101 @@ const ConfigGithub: React.FunctionComponent = () => {
   const gitHubAppInstallUrl = `https://github.com/apps/${appName}/installations/new/`
 
   return (
-    <div>
-      <h1>{t('Configure Dairy GitHub Connection.')}</h1>
+    <Container>
+      <Typography variant="h3">
+        {t('Configure Dairy GitHub Connection.')}
+      </Typography>
 
-      <h2>{t('1. Connect to GitHub')}</h2>
+      <Typography variant="h4">{t('1. Connect to GitHub')}</Typography>
 
-      <h3>{t('With GitHub App')}</h3>
-      <div>
-        <a href={gitHubAppInstallUrl}>Login with GitHub</a>
-      </div>
+      <Typography variant="h5">{t('With GitHub App')}</Typography>
+      <Box>
+        <Link href={gitHubAppInstallUrl}>Login with GitHub</Link>
+      </Box>
 
-      <h3>{t('OR with GitHub Token')}</h3>
-      <div>
-        <label>
-          {t('Token:')}
-          <input
-            type="password"
-            name="token"
-            value={config.github.token || ''}
+      <Typography variant="h5">{t('OR with GitHub Token')}</Typography>
+      <FormControl>
+        <InputLabel htmlFor="token-input">{t('Token:')}</InputLabel>
+        <Input
+          type="password"
+          id="token-input"
+          name="token"
+          aria-describedby="token-helper-text"
+          value={config.github.token || ''}
+          onChange={handleChange('github')}
+        />
+        <FormHelperText id="token-helper-text">
+          {t('Your GitHub token with access to your dairy repository.')}
+        </FormHelperText>
+      </FormControl>
+
+      <Typography variant="h4">{t('2. Select repository')}</Typography>
+      <Box>
+        <FormControl>
+          <InputLabel htmlFor="owner-input">{t('Owner:')}</InputLabel>
+          <Input
+            type="text"
+            id="owner-input"
+            name="owner"
+            aria-describedby="owner-helper-text"
+            value={config.github.owner || ''}
             onChange={handleChange('github')}
           />
-        </label>
-      </div>
+          <FormHelperText id="owner-helper-text">
+            {t('The repository owner name')}
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="repo-input">{t('Repository:')}</InputLabel>
+          <Input
+            type="text"
+            id="repo-input"
+            name="repo"
+            aria-describedby="repo-helper-text"
+            value={config.github.repo || ''}
+            onChange={handleChange('github')}
+          />
+          <FormHelperText id="repo-helper-text">
+            {t('The dairy repository name')}
+          </FormHelperText>
+        </FormControl>
+      </Box>
 
-      <h2>{t('2. Select repository')}</h2>
-      <div>
-        <div>
-          <label>
-            {t('Owner:')}
-            <input
-              type="text"
-              name="owner"
-              value={config.github.owner || ''}
-              onChange={handleChange('github')}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            {t('Repository:')}
-            <input
-              type="text"
-              name="repo"
-              value={config.github.repo || ''}
-              onChange={handleChange('github')}
-            />
-          </label>
-        </div>
-      </div>
+      <Typography variant="h4">{t('3. Name yourself for git')}</Typography>
+      <Box>
+        <Typography variant="h5">{t('Committer')}</Typography>
+        <FormControl>
+          <InputLabel htmlFor="author-input">{t('Name:')}</InputLabel>
+          <Input
+            type="text"
+            id="author-input"
+            name="author"
+            aria-describedby="author-helper-text"
+            value={config.committer.author || ''}
+            onChange={handleChange('committer')}
+          />
+          <FormHelperText id="author-helper-text">
+            {t('Your name for the git commit')}
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="email-input">{t('Email:')}</InputLabel>
+          <Input
+            type="text"
+            id="email-input"
+            name="email"
+            aria-describedby="email-helper-text"
+            value={config.committer.email || ''}
+            onChange={handleChange('committer')}
+          />
+          <FormHelperText id="email-helper-text">
+            {t('Your email for the git commit')}
+          </FormHelperText>
+        </FormControl>
+      </Box>
 
-      <h2>{t('3. Name yourself for git')}</h2>
-      <div>
-        <h4>{t('Committer')}</h4>
-        <div>
-          <label>
-            {t('Name:')}
-            <input
-              type="text"
-              name="author"
-              value={config.committer.author || ''}
-              onChange={handleChange('committer')}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            {t('Email:')}
-            <input
-              type="text"
-              name="email"
-              value={config.committer.email || ''}
-              onChange={handleChange('committer')}
-            />
-          </label>
-        </div>
-      </div>
-
-      <button onClick={handleSave}>{t('Save', { ns: 'general' })}</button>
-    </div>
+      <Button onClick={handleSave}>{t('Save', { ns: 'general' })}</Button>
+    </Container>
   )
 }
 
