aabfe38b3734fad7f319617d1c9305e819e8224d
Ilya Shestakov <siavol@gmail.com>
2024-09-24 00:48:34 +0300

    highlight selected config tab

--- src/components/config/config-github.tsx
+++ src/components/config/config-github.tsx
@@ -21,17 +21,27 @@ import {
 import PasswordIcon from '@mui/icons-material/Password'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import PersonIcon from '@mui/icons-material/Person'
-import { Outlet, useLoaderData, Link as RouterLink } from 'react-router-dom'
+import {
+  Outlet,
+  useLoaderData,
+  Link as RouterLink,
+  useLocation,
+} from 'react-router-dom'
 import { ConfigGithubData } from './config-actions'
 import { InstallGitHubApp, LoginToGitHubApp } from './github-auth'
 import GithubRepoSelect, { RepoValue } from './github-repo'
 
 const ConfigGithub: React.FunctionComponent = () => {
   const { repos } = useLoaderData() as ConfigGithubData
+  const location = useLocation()
   const [config, setConfig] = useState<Config>(loadConfig())
   const { t } = useTranslation(['config', 'general'])
 
   const drawerWidth = 240
+
+  const isLocation = (path: string): boolean => {
+    return location.pathname.endsWith(`/${path}`)
+  }
 
   const handleChange =
     (section: keyof Config) => (e: ChangeEvent<HTMLInputElement>) => {
@@ -91,7 +101,11 @@ const ConfigGithub: React.FunctionComponent = () => {
         <Box sx={{ overflow: 'auto' }}>
           <List>
             <ListItem key="auth" disablePadding>
-              <ListItemButton to="./auth" component={RouterLink}>
+              <ListItemButton
+                to="./auth"
+                component={RouterLink}
+                selected={isLocation('auth')}
+              >
                 <ListItemIcon>
                   <PasswordIcon />
                 </ListItemIcon>
@@ -99,7 +113,11 @@ const ConfigGithub: React.FunctionComponent = () => {
               </ListItemButton>
             </ListItem>
             <ListItem key="repo" disablePadding>
-              <ListItemButton to="./repo" component={RouterLink}>
+              <ListItemButton
+                to="./repo"
+                component={RouterLink}
+                selected={isLocation('repo')}
+              >
                 <ListItemIcon>
                   <GitHubIcon />
                 </ListItemIcon>
@@ -107,7 +125,11 @@ const ConfigGithub: React.FunctionComponent = () => {
               </ListItemButton>
             </ListItem>
             <ListItem key="author" disablePadding>
-              <ListItemButton to="./author" component={RouterLink}>
+              <ListItemButton
+                to="./author"
+                component={RouterLink}
+                selected={isLocation('author')}
+              >
                 <ListItemIcon>
                   <PersonIcon />
                 </ListItemIcon>
@@ -120,79 +142,6 @@ const ConfigGithub: React.FunctionComponent = () => {
       <Box sx={{ flexGrow: 1, p: 3, paddingLeft: `${drawerWidth}px` }}>
         <Toolbar />
         <Outlet></Outlet>
-
-        {/* <Typography variant="h3">
-          {t('Configure Dairy GitHub Connection.')}
-        </Typography> */}
-
-        {/* <Typography variant="h4">{t('1. Connect to GitHub')}</Typography> */}
-
-        {/* <Typography variant="h5">{t('With GitHub App')}</Typography>
-        <Box>
-          <InstallGitHubApp />
-          <Typography variant="body1" display="inline">
-            {t(' or ')}
-          </Typography>
-          <LoginToGitHubApp />
-        </Box>
-
-        <Typography variant="h5">{t('OR with GitHub Token')}</Typography>
-        <FormControl>
-          <InputLabel htmlFor="token-input">{t('Token:')}</InputLabel>
-          <Input
-            type="password"
-            id="token-input"
-            name="token"
-            aria-describedby="token-helper-text"
-            value={config.github.auth.token || ''}
-            onChange={handleAuthTokenChange}
-          />
-          <FormHelperText id="token-helper-text">
-            {t('Your GitHub token with access to your dairy repository.')}
-          </FormHelperText>
-        </FormControl>
-
-        <Typography variant="h4">{t('2. Select repository')}</Typography>
-        <GithubRepoSelect
-          repos={repos}
-          value={config.github}
-          onChange={handleRepoNameChange}
-        />
-
-        <Typography variant="h4">{t('3. Name yourself for git')}</Typography>
-        <Box>
-          <Typography variant="h5">{t('Committer')}</Typography>
-          <FormControl>
-            <InputLabel htmlFor="author-input">{t('Name:')}</InputLabel>
-            <Input
-              type="text"
-              id="author-input"
-              name="author"
-              aria-describedby="author-helper-text"
-              value={config.committer.author || ''}
-              onChange={handleChange('committer')}
-            />
-            <FormHelperText id="author-helper-text">
-              {t('Your name for the git commit')}
-            </FormHelperText>
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="email-input">{t('Email:')}</InputLabel>
-            <Input
-              type="text"
-              id="email-input"
-              name="email"
-              aria-describedby="email-helper-text"
-              value={config.committer.email || ''}
-              onChange={handleChange('committer')}
-            />
-            <FormHelperText id="email-helper-text">
-              {t('Your email for the git commit')}
-            </FormHelperText>
-          </FormControl>
-        </Box>
-
-        <Button onClick={handleSave}>{t('Save', { ns: 'general' })}</Button> */}
       </Box>
     </>
   )
