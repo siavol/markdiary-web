5442970a6a51fc50b7e5b96604cf7042bbd21a95
siavol <siavol@gmail.com>
2024-10-08 00:39:34 +0300

    add spinner when loading app (#79)

---
+++ src/components/global-loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GlobalLoader: React.FunctionComponent = () => {
+  return (
+    <div id="loading-spinner">
+      <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default GlobalLoader
